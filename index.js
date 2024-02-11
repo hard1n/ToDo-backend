669e500df8698c87120bb22293156751b5224a1f
Hardin <hardisonpaulino@gmail.com>
2024-02-11 16:42:05 -0400

    feat: .env added

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+require("dotenv").config();
 const http = require("http");
 
 function requesController() {
   console.log("Hello");
 }
 
+const PORT = process.env.PORT;
+
 // Create server
 const server = http.createServer(requesController);
 
-server.listen(4000);
+server.listen(PORT, () => {
+  console.log(`App running in port: ${PORT}`);
+});
